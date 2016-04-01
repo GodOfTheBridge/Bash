1561c57b13c0eea79172d399fecc91a680832c3c
GodOfTheBridge <Ikonnikov.Pavel.S@gmail.com>
2016-04-01 18:56:22 +0500

    Создание нового проекта

---
+++ app/src/main/java/com/gotb/bash/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gotb.bash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
